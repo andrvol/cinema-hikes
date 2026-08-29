f8e95c4ccb1af48f720a1ad9b75a7aa79a5a2222
Egor <arnoldtagirov1@gmail.com>
2026-08-29 23:10:11 +0200

    added antd theme

---
+++ CinemaHikes/frontend/src/app/providers/AntdThemeProvider.tsx
@@ -0,0 +1,24 @@
+import {ConfigProvider,theme} from 'antd';
+import {ReactNode} from 'react';
+
+export const AntdThemeProvider = ({children}:{children:ReactNode}) => {
+    return(
+        <ConfigProvider theme={{algorithm:theme.darkAlgorithm,token:{
+            colorPrimary: '#E50914',
+            colorBgBase: '#141414',
+            colorTextBase: '#ffffff',
+            borderRadius: 6,
+        },
+        components:{Layout:{
+            headerBg: '#00000',
+            bodyBg: '#141414',
+            footerBg: '#00000',
+        },
+        Menu: {
+            darkItemBg: '#00000',
+        }
+      }
+    }}
+    >{children}</ConfigProvider>
+    )
+}
